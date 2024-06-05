5298c7fbb55af6670d310645352a5e05d0775209
PranavDalvi9 <pranavdalvi9@gmail.com>
2024-06-05 23:00:08 +0530

    first commit

---
+++ src/components/side-nav.jsx
@@ -0,0 +1,94 @@
+'use client';
+
+import React, { useState } from 'react';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { SIDENAV_ITEMS } from '@/constants';
+import { Icon } from '@iconify/react';
+
+const SideNav = () => {
+    console.log(SIDENAV_ITEMS);
+  return (
+    <div className="md:w-60 bg-white text-black h-screen flex-1 fixed border-r border hidden md:flex">
+      <div className="flex flex-col space-y-6 w-full">
+        <Link
+          href="/"
+          className="flex flex-row space-x-3 items-center justify-center md:justify-start md:px-6 border-b border-black h-12 w-full"
+        >
+          <span className="h-7 w-7 bg-black rounded-lg" />
+          <span className="font-bold text-xl hidden md:flex">Logo</span>
+        </Link>
+
+        <div className="flex flex-col space-y-2 md:px-6">
+          {SIDENAV_ITEMS.map((item, idx) => {
+            return <MenuItem key={idx} item={item} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SideNav;
+
+const MenuItem = ({ item }) => {
+  const pathname = usePathname();
+  const [subMenuOpen, setSubMenuOpen] = useState(false);
+  const toggleSubMenu = () => {
+    setSubMenuOpen(!subMenuOpen);
+  };
+
+  return (
+    <div>
+      {item.submenu ? (
+        <>
+          <button
+            onClick={toggleSubMenu}
+            className={`text-black flex flex-row items-center p-2 rounded-lg hover-bg-black w-full justify-between hover:bg-zinc-400 ${
+              pathname.includes(item.path) ? 'bg-zinc-100' : ''
+            }`}
+          >
+            <div className="flex flex-row space-x-4 items-center">
+              {item.icon}
+              <span className="font-semibold text-xl flex">{item.title}</span>
+            </div>
+
+            <div className={`${subMenuOpen ? 'rotate-180' : ''} flex`}>
+              <Icon icon="lucide:chevron-down" width="24" height="24" />
+            </div>
+          </button>
+
+          {subMenuOpen && (
+            <div className="my-2 ml-12 flex flex-col space-y-4">
+              {item.subMenuItems?.map((subItem, idx) => {
+                return (
+                  <Link
+                    key={idx}
+                    href={subItem.path}
+                    className={`${
+                      subItem.path === pathname ? 'font-bold' : ''
+                    }`}
+                  >
+                    <span>{subItem.title}</span>
+                  </Link>
+                );
+              })}
+            </div>
+          )}
+        </>
+      ) : (
+        <Link
+          href={item.path}
+          className={`flex flex-row space-x-4 items-center p-2 rounded-lg hover:bg-zinc-400 ${
+            item.path === pathname ? 'bg-zinc-100' : ''
+          }`}
+        >
+          {item.icon}
+          <span className="font-semibold text-xl flex">{item.title}</span>
+        </Link>
+      )}
+    </div>
+  );
+};
